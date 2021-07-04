17617f194e28c533638d8917c1f27b54418ef5d3
viktor <vikhulei@gmail.com>
2021-07-04 22:31:42 +0300

    started the router

---
+++ src/About.js
@@ -0,0 +1,7 @@
+const About = () => {
+    return <div>
+        Hello from About
+    </div>
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import About from "./About"
 
 function App() {
   return (
     <div className="App">
-          Learn React
+      <Router>
+          <h1>This is Home page</h1>
+          <Link to="/about">About</Link>
+      <Switch>
+        <Route path="/about" component={About}/>
+      </Switch>
+      </Router>
     </div>
+    
   );
 }
 
